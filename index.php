3fe7450fa2bd9b5c704128a38548c0c09965c03f
Enric Pineda <enricpineda@gmail.com>
2021-06-06 14:17:52 +0200

    primer commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Associació Catalana d'Esperanto - Kataluna Esperanto Asocio</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="style.css?v=1.0">
+
+</head>
+
+<body>
+<header>
+<div id="logo-xarxes" class="bloc">
+<div id="logo">Logo</div>
+<div id="idiomes-xarxes-correu">
+  <div id="idiomes-cap">Idiomes</div>
+  <div id="xarxes-cap">Xarxes socials</div>
+  <div id="correu-cap">Accés al correu</div>
+</div>
+</div>
+</header>
+<nav>Menú principal</nav>
+<div id="cercador">Cercador</div>
+<div id="footer"></div>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
